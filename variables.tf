2c7e1fb1a3f065d7fb5bbf8b527f5835f2f22bab
AtharvaBondreScoutflo <115654880+AtharvaBondreScoutflo@users.noreply.github.com>
2025-03-03 20:33:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,7 +10,7 @@ variable "region" {
 variable "cluster_name" {
   description = "Cluster name"
   type = string
-  default = "ingresstesting-1"
+  default = "scoutflo-test"
 }
 
 variable "instance_type" {
