9ccedbfeebb78e53c465a961c7b11906c0a268e9
vedant-8680 <146105565+vedant-8680@users.noreply.github.com>
2025-09-10 15:39:50 +0530

    Update main.tf
    Added a new logic for azs to pick either "3" or the minimum number of available az on the selected region.

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "vpc" {
   version = "3.19.0"
   name    = "scoutflo-vpc-${random_string.suffix.result}"
   cidr    = "10.0.0.0/16"
-  azs     = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs     = slice(data.aws_availability_zones.available.names, 0, min(3, length(data.aws_availability_zones.available.names)))
   public_subnets = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_dns_hostnames = true
   public_subnet_tags = {
